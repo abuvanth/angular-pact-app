7cf4a376bff1dca66652a403fa4f985a24c8d082
Syed Abuthahir <abu@Syeds-MBP.domain.name>
2022-06-08 11:31:43 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+declare var require: any
+const pact = require("pact-lang-api")
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pact-test';
+  constructor(){
+    console.log(pact.crypto.genKeyPair())
+  }
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -58,7 +58,14 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js';  // Included with Angular CLI.
-
+import * as Buffer from '../node_modules/buffer/index';
+(window as any).global = window;
+(window as any).process = {};
+(window as any).process = window;
+(window as any).process.browser = true;
+(window as any).process.version = '';
+(window as any).process.versions = { node: false };
+(window as any).global.Buffer = Buffer.Buffer;
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
